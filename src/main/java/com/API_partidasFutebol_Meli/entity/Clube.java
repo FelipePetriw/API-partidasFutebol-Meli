798acbb3627faaa0c798f1937cc94cbc0af19e13
Felipe-Petriw <felipe.slopes@mercadolivre.com>
2025-07-08 19:42:58 -0300

    criacao da entidade clube com atributos e comandos get e set

---
+++ src/main/java/com/API_partidasFutebol_Meli/entity/Clube.java
@@ -0,0 +1,68 @@
+package com.API_partidasFutebol_Meli.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "clubes", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"nome", "sigla_estado"})
+})
+public class Clube {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(name = "sigla_estado", nullable = false, length = 2)
+    private String siglaEstado;
+
+    @Column(name = "data_criacao", nullable = false)
+    private LocalDate dataCriacao;
+
+    @Column(nullable = false)
+    private Boolean ativo = true;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSiglaEstado() {
+        return siglaEstado;
+    }
+
+    public void setSiglaEstado(String siglaEstado) {
+        this.siglaEstado = siglaEstado;
+    }
+
+    public LocalDate getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(LocalDate dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public Boolean getAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(Boolean ativo) {
+        this.ativo = ativo;
+    }
+}
