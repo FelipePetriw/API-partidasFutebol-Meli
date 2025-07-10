43d098f1946d3161b1064c3fdedfbfa0a96601a1
Felipe-Petriw <felipe.slopes@mercadolivre.com>
2025-07-10 14:15:38 -0300

    Correcao da importacao da anotacao Pattern

--- src/main/java/com/API_partidasFutebol_Meli/dto/ClubeRequestDTO.java
+++ src/main/java/com/API_partidasFutebol_Meli/dto/ClubeRequestDTO.java
@@ -3,9 +3,10 @@ package com.API_partidasFutebol_Meli.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import org.antlr.v4.runtime.misc.NotNull;
-import org.hibernate.annotations.processing.Pattern;
+
 
 import java.time.LocalDate;
 
@@ -13,7 +14,9 @@ public record ClubeRequestDTO(
         @NotBlank @Size(min = 2)
         String nome,
 
-        @NotBlank @Pattern(regexp = "AC|AL|AP|AM|BA|CE|DF|ES|GO|MA|MT|MS|MG|PA|PB|PR|PE|PI|RJ|RN|RS|RO|RR|SC|SP|SE|TO", message = "Estado inválido")
+        @NotBlank @Pattern(
+                regexp = "(?i)^(AC|AL|AP|AM|BA|CE|DF|ES|GO|MA|MT|MS|MG|PA|PB|PR|PE|PI|RJ|RN|RS|RO|RR|SC|SP|SE|TO)$",
+                message = "Estado inválido")
         String siglaEstado,
 
         @NotNull @PastOrPresent
