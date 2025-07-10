4e19c0f35528f164c25d17b87897ab229a9f059c
Felipe-Petriw <felipe.slopes@mercadolivre.com>
2025-07-10 11:43:32 -0300

    criacao da service clube

---
+++ src/main/java/com/API_partidasFutebol_Meli/service/ClubeService.java
@@ -0,0 +1,34 @@
+package com.API_partidasFutebol_Meli.service;
+
+import com.API_partidasFutebol_Meli.dto.ClubeRequestDTO;
+import com.API_partidasFutebol_Meli.dto.ClubeResponseDTO;
+import com.API_partidasFutebol_Meli.entity.Clube;
+import com.API_partidasFutebol_Meli.repository.ClubeRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class ClubeService {
+
+    private final ClubeRepository repository;
+
+    public ClubeService(ClubeRepository repository) {
+        this.repository = repository;
+    }
+
+    @Transactional
+    public ClubeResponseDTO criar(ClubeRequestDTO dto) {
+        repository.findByNomeAndSiglaEstado(dto.nome(),  dto.siglaEstado()).ifPresent(c -> {
+            throw new RecursoDuplicadoException("Já existe um clube com este nome neste estado.");
+        });
+
+        Clube clube = new Clube();
+        clube.setNome(dto.nome());
+        clube.setSiglaEstado(dto.siglaEstado());
+        clube.setDataCriacao(dto.dataCriacao());
+        clube.setAtivo(true);
+
+        Clube salvo = repository.save(clube);
+        return new ClubeResponseDTO(salvo.getId(), salvo.getNome(), salvo.getSiglaEstado(), salvo.getDataCriacao(), salvo.getAtivo());
+    }
+}
