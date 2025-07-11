4e202354a2fc751ac7fdf179231f53c8d4779e79
Felipe-Petriw <felipe.slopes@mercadolivre.com>
2025-07-11 19:20:24 -0300

    Criacao do metodo de atualizar clube com validacao das exception

--- src/main/java/com/API_partidasFutebol_Meli/service/ClubeService.java
+++ src/main/java/com/API_partidasFutebol_Meli/service/ClubeService.java
@@ -2,11 +2,15 @@ package com.API_partidasFutebol_Meli.service;
 
 import com.API_partidasFutebol_Meli.dto.ClubeRequestDTO;
 import com.API_partidasFutebol_Meli.dto.ClubeResponseDTO;
+import com.API_partidasFutebol_Meli.dto.ClubeUpdateDTO;
 import com.API_partidasFutebol_Meli.entity.Clube;
 import com.API_partidasFutebol_Meli.exception.RecursoDuplicadoException;
 import com.API_partidasFutebol_Meli.repository.ClubeRepository;
+import org.apache.coyote.BadRequestException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
 
 @Service
 public class ClubeService {
@@ -31,5 +35,25 @@ public class ClubeService {
 
         Clube salvo = repository.save(clube);
         return new ClubeResponseDTO(salvo.getId(), salvo.getNome(), salvo.getSiglaEstado(), salvo.getDataCriacao(), salvo.getAtivo());
+    }
+
+    @Transactional
+    public ClubeResponseDTO atualizar(Long id, ClubeUpdateDTO dto) throws BadRequestException {
+        Clube clube = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Clube não encontrado."));
+
+        if (clube.getNome().equals(dto.nome()) || !clube.getSiglaEstado().equals(dto.siglaEstado()))
+            repository.findByNomeAndSiglaEstado(dto.nome(), dto.siglaEstado()).ifPresent(c -> {
+                throw new RecursoDuplicadoException("Já existe um clube com este nome neste estado.");
+            });
+
+        if (dto.dataCriacao().isAfter(LocalDate.now())) {
+            throw new BadRequestException("Data de criação não pode ser futura.");
+        }
+
+        clube.setNome(dto.nome());
+        clube.setSiglaEstado(dto.siglaEstado());
+        clube.setDataCriacao(dto.dataCriacao());
+
+        return new ClubeResponseDTO(clube.getId(), clube.getNome(), clube.getSiglaEstado(),  clube.getDataCriacao(), clube.getAtivo());
     }
 }
