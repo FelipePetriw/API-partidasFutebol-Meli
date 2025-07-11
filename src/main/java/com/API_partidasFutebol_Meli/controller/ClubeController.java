8528b60a637d7eb71ce4ed66167730be73df1c76
Felipe-Petriw <felipe.slopes@mercadolivre.com>
2025-07-11 19:43:12 -0300

    Atualizacao do controller para o metodo editar, criacao de exception notFound e BadRequest e atualizacao da GlobalExceptionHandler.

--- src/main/java/com/API_partidasFutebol_Meli/controller/ClubeController.java
+++ src/main/java/com/API_partidasFutebol_Meli/controller/ClubeController.java
@@ -2,13 +2,12 @@ package com.API_partidasFutebol_Meli.controller;
 
 import com.API_partidasFutebol_Meli.dto.ClubeRequestDTO;
 import com.API_partidasFutebol_Meli.dto.ClubeResponseDTO;
+import com.API_partidasFutebol_Meli.dto.ClubeUpdateDTO;
 import com.API_partidasFutebol_Meli.service.ClubeService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/clubes")
@@ -24,5 +23,11 @@ public class ClubeController {
     public ResponseEntity<ClubeResponseDTO> criar(@RequestBody @Valid ClubeRequestDTO dto) {
         var response = service.criar(dto);
         return ResponseEntity.status(201).body(response);
+    }
+
+    @PutMapping("/id")
+    public ResponseEntity<ClubeResponseDTO> atualizar(@PathVariable Long id, @RequestBody @Valid ClubeUpdateDTO dto) {
+        var response = service.atualizar(id, dto);
+        return ResponseEntity.ok(response);
     }
 }
--- src/main/java/com/API_partidasFutebol_Meli/exception/GlobalExceptionHandler.java
+++ src/main/java/com/API_partidasFutebol_Meli/exception/GlobalExceptionHandler.java
@@ -8,13 +8,23 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(RecursoDuplicadoException.class)
-    public ResponseEntity<String> conflito(RecursoDuplicadoException ex) {
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
-    }
+  @ExceptionHandler(RecursoDuplicadoException.class)
+  public ResponseEntity<String> conflito(RecursoDuplicadoException ex) {
+    return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
+  }
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> erroGenerico(Exception ex) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro inesperado: " + ex.getMessage());
-    }
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity<String> erroGenerico(Exception ex) {
+    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro inesperado: " + ex.getMessage());
+  }
+
+  @ExceptionHandler(ResourceNotFoundException.class)
+  public ResponseEntity<String> notFound(ResourceNotFoundException ex) {
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+  }
+
+  @ExceptionHandler(BadRequestException.class)
+  public ResponseEntity<String> badRequest(BadRequestException ex) {
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+  }
 }
