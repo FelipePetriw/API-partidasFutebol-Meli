2ccb5eccd8191a4607a25216181320d155010223
Felipe-Petriw <felipe.slopes@mercadolivre.com>
2025-07-14 19:04:10 -0300

    criacao do repository da partida

---
+++ src/main/java/com/API_partidasFutebol_Meli/repository/PartidaRepository.java
@@ -0,0 +1,19 @@
+package com.API_partidasFutebol_Meli.repository;
+
+import com.API_partidasFutebol_Meli.entity.Clube;
+import com.API_partidasFutebol_Meli.entity.Partida;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+
+public interface PartidaRepository extends JpaRepository<Partida, Long> {
+
+    boolean existsByClubeMandanteOrClubeVisitanteAndDataHoraBetween(
+            Clube clubeMandante, Clube clubeVisitante,
+            LocalDateTime inicio, LocalDateTime fim
+    );
+
+    boolean existsByEstadioAndDataHoraBetween(
+            Estadio estadio, LocalDateTime inicio, LocalDateTime fim
+    );
+}
