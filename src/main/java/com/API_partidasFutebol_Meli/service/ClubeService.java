d8be06c6db9a6cc2fbdd34fb73f1a592b209ae0b
Felipe-Petriw <felipe.slopes@mercadolivre.com>
2025-07-15 13:29:10 -0300

    inclusão do cadastro de partida na service

--- src/main/java/com/API_partidasFutebol_Meli/service/ClubeService.java
+++ src/main/java/com/API_partidasFutebol_Meli/service/ClubeService.java
@@ -1,14 +1,13 @@
 package com.API_partidasFutebol_Meli.service;
 
-import com.API_partidasFutebol_Meli.dto.ClubeFiltroDTO;
-import com.API_partidasFutebol_Meli.dto.ClubeRequestDTO;
-import com.API_partidasFutebol_Meli.dto.ClubeResponseDTO;
-import com.API_partidasFutebol_Meli.dto.ClubeUpdateDTO;
+import com.API_partidasFutebol_Meli.dto.*;
 import com.API_partidasFutebol_Meli.entity.Clube;
+import com.API_partidasFutebol_Meli.entity.Partida;
 import com.API_partidasFutebol_Meli.exception.RecursoDuplicadoException;
 import com.API_partidasFutebol_Meli.exception.ResourceNotFoundException;
+import com.API_partidasFutebol_Meli.exception.BadRequestException;
 import com.API_partidasFutebol_Meli.repository.ClubeRepository;
-import org.apache.coyote.BadRequestException;
+import com.API_partidasFutebol_Meli.repository.PartidaRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
@@ -21,9 +20,13 @@ import java.time.LocalDate;
 public class ClubeService {
 
     private final ClubeRepository repository;
+    private final ClubeRepository clubeRepository;
+    private final PartidaRepository partidaRepository;
 
-    public ClubeService(ClubeRepository repository) {
+    public ClubeService(ClubeRepository repository, ClubeRepository clubeRepository, PartidaRepository partidaRepository) {
         this.repository = repository;
+        this.clubeRepository = clubeRepository;
+        this.partidaRepository = partidaRepository;
     }
 
     @Transactional
@@ -85,7 +88,7 @@ public class ClubeService {
 
     @Transactional
     public Page<ClubeResponseDTO> listar(ClubeFiltroDTO filtro, Pageable pageable) {
-        Specification<Clube> spec = Specification.where(null);
+        Specification<Clube> spec = ((root, query, criteriaBuilder) -> null);
 
         if(filtro.nome() != null && !filtro.nome().isBlank()) {
             spec = spec.and((root, query, criteriaBuilder)
@@ -110,5 +113,60 @@ public class ClubeService {
                 clube.getAtivo()
 
         ));
+    }
+
+    @Transactional
+    public PartidaResponseDTO cadastrar(PartidaRequestDTO dto) {
+        if (dto.clubeMandante().equals(dto.clubeVisitante())) {
+            throw new BadRequestException("Clube mandante e visitante não podem ser iguais.");
+        }
+
+        Clube mandante = clubeRepository.findById(dto.clubeMandanteId())
+                .orElseThrow(() -> new ResourceNotFoundException("Clube mandante não encontrado."));
+        Clube visitante = clubeRepository.findById(dto.clubeVisitanteId())
+                .orElseThrow(() -> new ResourceNotFoundException("Clube visitante não encontrado."));
+        Estadio estadio = estadioRepository.findById(dto.estadioId())
+                .orElseThrow(() -> new ResourceNotFoundException("Estádio não encontrado."));
+
+        if(!mandante.getAtivo() || !visitante.getAtivo()){
+            throw new ConflictException("Partida não pode ser cadastrada com clube inativo.");
+        }
+
+        if (dto.dataHora().isBefore(mandante.getDataCriacao()) || dto.dataHora().isBefore(visitante.getDataCriacao())) {
+            throw new ConflictException("Data da partida anterior à criação de um dos clubes.");
+        }
+
+        var inicio = dto.dataHora().minusHours(48);
+        var fim = dto.dataHora().plusHours(48);
+        boolean conflitoHorario = partidaRepository.existsByClubeMandanteOrClubeVisitanteAndDataHoraBetween(mandante, visitante, inicio, fim);
+
+        if (conflitoHorario) {
+            throw new ConflictException("Um dos clubes já tem partida próxima (menos de 48h");
+        }
+
+        boolean estadioOcupado = partidaRepository.existsByEstadioAndDataHoraBetween(
+                estadio, dto.dataHora().toLocalDate().atStartOfDay(), dto.dataHora().atTime(23, 59));
+        if (estadioOcupado) {
+            throw new ConflictException("Já existe partida nesse estádio no mesmo dia.");
+        }
+
+        Partida partida = new Partida();
+        partida.setClubeMandante(mandante);
+        partida.setClubeVisitante(visitante);
+        partida.setEstadio(estadio);
+        partida.setDataHora(dto.dataHora().atStartOfDay());
+        partida.setGolsMandante(dto.golsMandante());
+        partida.setGolsVisitante(dto.golsVisitante());
+
+        var salvo = partidaRepository.save(partida);
+        return new PartidaResponseDTO(
+                salvo.getId(),
+                mandante.getNome(),
+                visitante.getNome(),
+                estadio.getNome(),
+                salvo.getDataHora(),
+                salvo.getGolsMandante(),
+                salvo.getGolsVisitante()
+        );
     }
 }
