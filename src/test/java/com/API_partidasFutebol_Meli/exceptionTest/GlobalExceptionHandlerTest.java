c5c3f6314df023605797aff1c61f05c7e0f32746
Felipe-Petriw <felipe.slopes@mercadolivre.com>
2025-07-24 11:29:45 -0300

    inclusao de testes sobre a GlobalHandlerException

--- src/test/java/com/API_partidasFutebol_Meli/exceptionTest/GlobalExceptionHandlerTest.java
+++ src/test/java/com/API_partidasFutebol_Meli/exceptionTest/GlobalExceptionHandlerTest.java
@@ -1,8 +1,6 @@
 package com.API_partidasFutebol_Meli.exceptionTest;
 
-import com.API_partidasFutebol_Meli.exception.BadRequestException;
-import com.API_partidasFutebol_Meli.exception.GlobalExceptionHandler;
-import com.API_partidasFutebol_Meli.exception.ResourceNotFoundException;
+import com.API_partidasFutebol_Meli.exception.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.ResponseEntity;
 
@@ -24,5 +22,26 @@ public class GlobalExceptionHandlerTest {
         ResponseEntity<String> response = handler.badRequest(new BadRequestException("ruim"));
         assertEquals(400, response.getStatusCodeValue());
         assertEquals("ruim", response.getBody());
+    }
+
+    @Test
+    void deveLidarComRecursoDuplicado() {
+        ResponseEntity<String> response = handler.duplicateResource(new RecursoDuplicadoException("Duplicado"));
+        assertEquals(409, response.getStatusCodeValue());
+        assertEquals("Duplicado", response.getBody());
+    }
+
+    @Test
+    void deveLidarComConflito() {
+        ResponseEntity<String> response = handler.conflict(new ConflictException("Conflito detectado"));
+        assertEquals(409, response.getStatusCodeValue());
+        assertEquals("Conflito detectado", response.getBody());
+    }
+
+    @Test
+    void deveLidarComErroGenerico() {
+        ResponseEntity<String> response = handler.genericError(new RuntimeException("Falha Interna"));
+        assertEquals(500, response.getStatusCodeValue());
+        assertEquals("Erro inesperado: Falha Interna", response.getBody());
     }
 }
